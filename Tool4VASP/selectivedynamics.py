7c08fc785368efff7384537018813f07b6cc19ac
Machika1110 <naitomachika1110@gmail.com>
2023-09-18 14:56:46 +0900

    Change lattice parameter & add selectivedynamics

---
+++ Tool4VASP/selectivedynamics.py
@@ -0,0 +1,47 @@
+# this program can be used for lectangier (α=β=γ=90°)
+# "range" decides range of not moving
+range_z = 0.5
+
+# read POSCAR
+with open("POSCAR", "r") as f:
+    lines = f.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace("\n", "")
+
+# insert "Selectie dynamics"
+lines.insert(7, "Selective dynamics")
+
+# separate lattice & position
+lattice_para = lines[0:9]
+position = lines[9:-1]
+
+for i in range(len(position)):
+    position[i] = position[i].split()
+
+print(position)
+
+# get z position and z length
+position_z = []
+for i in range(len(position)):
+    position_z.append(float(position[i][2]))
+zmax = max(position_z)
+zmin = min(position_z)
+zlength = zmax-zmin
+
+# set capability (TTT or FFF)
+move = []
+for i in range(len(position)):
+    if position_z[i] <= zlength*range_z+zmin:
+        move.append("F F F") # don't move
+    if position_z[i] > zlength*range_z+zmin:
+        move.append("T T T") # can move
+
+
+# write POSCAR_selected
+with open("POSCAR_selected", "w") as f:
+    for i in range(len(lattice_para)):
+        print(lattice_para[i], file = f)
+    for i in range(len(position)):
+        for j in range(3):
+            f.write("{:.6f} ".format(float(position[i][j])))
+        print(move[i], file=f)
